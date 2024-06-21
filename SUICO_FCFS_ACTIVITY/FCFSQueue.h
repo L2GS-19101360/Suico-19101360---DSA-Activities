cd7f37cbfa5319e320f43f5090202918442f0d8b
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-06-21 10:58:20 +0800

    Initial FCFS Queue Completion

--- SUICO_FCFS_ACTIVITY/FCFSQueue.h
+++ SUICO_FCFS_ACTIVITY/FCFSQueue.h
@@ -2,4 +2,35 @@
 #define FCFS_QUEUE
 #include <stdbool.h>
 
+typedef struct {
+	char processId;
+	int arrivalTime;
+	int burstTime;
+	int executionTime;
+}ProcessData;
+
+typedef struct node {
+	ProcessData process;
+	struct node *next;
+}ProcessNode, *ProcessNodePtr;
+
+typedef struct {
+	ProcessNodePtr front;
+	ProcessNodePtr rear;
+}FCFSQueue;
+
+FCFSQueue createFCFS();
+void initFCFS(FCFSQueue *fq);
+ProcessData emptyProcess();
+
+void populateFCFSQueue(FCFSQueue *fq);
+void displayFCFSQueue(FCFSQueue fq);
+
+bool EnqueueProcess(FCFSQueue *fq, ProcessData p);
+ProcessData Front(FCFSQueue fq);
+bool DequeueProcess(FCFSQueue *fq);
+ProcessData Rear(FCFSQueue fq);
+
+bool isEmpty(FCFSQueue fq);
+
 #endif
