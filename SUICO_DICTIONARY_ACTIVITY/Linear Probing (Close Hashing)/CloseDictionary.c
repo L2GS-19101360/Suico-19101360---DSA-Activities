ee387d719ea800f94365c4a26d280833bddf58ec
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-07-03 10:17:45 +0800

    ADT Dictionary Linear Probing Completion

--- SUICO_DICTIONARY_ACTIVITY/Linear Probing (Close Hashing)/CloseDictionary.c
+++ SUICO_DICTIONARY_ACTIVITY/Linear Probing (Close Hashing)/CloseDictionary.c
@@ -2,8 +2,54 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "CloseDictionary.h"
 
+bool memberHashValue(char string[], CLOSEDICT cd)
+{
+    unsigned int hashIndex = hashValue(string, cd);
+    int x;
+    for (x = 0; strcmp(cd.hashTable[hashIndex].string, EMPTY) != 0; hashIndex = (hashIndex + (x * x)) % cd.size, x++) {
+        if (strcmp(cd.hashTable[hashIndex].string, string) == 0) {
+            return true;
+        }
+        if (strcmp(cd.hashTable[hashIndex].string, EMPTY) == 0) {
+            break;
+        }
+    }
+    return false;
+}
+void deleteHashValue(char string[], CLOSEDICT *cd)
+{
+	unsigned int hashIndex = hashValue(string, *cd);
+	int x;
+	for (x = 0; strcmp(string, cd->hashTable[hashIndex].string) != 0; hashIndex = (hashIndex + (x*x))%cd->size, x++){}
+	strcpy(cd->hashTable[hashIndex].string, DELETED);
+	cd->count--;
+}
+void resizeCloseDictionary(CLOSEDICT *cd)
+{
+	int newSize = cd->size * 2, x;
+	HASHTABLE *newHashTable = (HASHTABLE *)malloc(newSize * sizeof(HASHTABLE));
+	if (newHashTable != NULL) {
+		for (x = 0; x < newSize; x++) {
+            strcpy(newHashTable[x].string, EMPTY);
+        }
+
+		for (x = 0; x < cd->size; x++) {
+	        if (strcmp(cd->hashTable[x].string, EMPTY) != 0 && strcmp(cd->hashTable[x].string, DELETED) != 0) {
+	            unsigned int hashIndex = hashValue(cd->hashTable[x].string, *cd);
+	            int i;
+	            for (i = 0; strcmp(newHashTable[hashIndex].string, EMPTY) != 0 && strcmp(newHashTable[hashIndex].string, DELETED) != 0; i++, hashIndex = (hashIndex + i * i) % newSize) {}
+	            strcpy(newHashTable[hashIndex].string, cd->hashTable[x].string);
+	        }
+	    }
+	
+	    free(cd->hashTable);
+	    cd->hashTable = newHashTable;
+	    cd->size = newSize;
+	}
+}
 void printHashTable(CLOSEDICT cd)
 {
 	int x;
@@ -15,14 +61,14 @@ void printHashTable(CLOSEDICT cd)
 }
 void insertHashValue(char string[], CLOSEDICT *cd)
 {
-	unsigned int hashIndex;
-	int x;
-	if (!isOverThreshold(*cd)) {
-		hashIndex = hashValue(string, *cd);
-		for (x = 0; strcmp(cd->hashTable[hashIndex].string, EMPTY) != 0 && strcmp(cd->hashTable[hashIndex].string, DELETED) != 0; x++, hashIndex = (hashValue(string, *cd) + (x*x))%cd->size) {}
-		strcpy(cd->hashTable[hashIndex].string, string);
-		cd->count++;
-	}
+    if (isOverThreshold(*cd)) {
+        resizeCloseDictionary(cd);
+    }
+    unsigned int hashIndex = hashValue(string, *cd);
+    int x;
+    for (x = 0; strcmp(cd->hashTable[hashIndex].string, EMPTY) != 0 && strcmp(cd->hashTable[hashIndex].string, DELETED) != 0; x++, hashIndex = (hashIndex + x * x) % cd->size) {}
+    strcpy(cd->hashTable[hashIndex].string, string);
+    cd->count++;
 }
 unsigned int hashValue(char string[], CLOSEDICT cd)
 {
--- SUICO_DICTIONARY_ACTIVITY/Linear Probing (Close Hashing)/main.c
+++ SUICO_DICTIONARY_ACTIVITY/Linear Probing (Close Hashing)/main.c
@@ -13,12 +13,20 @@ int main(int argc, char *argv[]) {
 	insertHashValue("Orange", &cd);
 	insertHashValue("Violet", &cd);
 	insertHashValue("Green", &cd);
-	printHashTable(cd);
 	insertHashValue("Black", &cd);
 	printHashTable(cd);
-//	insertHashValue("White", &cd);
-//	printHashTable(cd);
-//	insertHashValue("Brown", &cd);
-//	printHashTable(cd);
+	printf("After Resizing Hash Table:\n");
+	resizeCloseDictionary(&cd);
+	insertHashValue("White", &cd);
+	insertHashValue("Brown", &cd);
+	insertHashValue("Pink", &cd);
+	insertHashValue("Gray", &cd);
+	printHashTable(cd);
+	deleteHashValue("Red", &cd);
+	deleteHashValue("Blue", &cd);
+	printHashTable(cd);
+	printf("Member Yellow? %s\n", memberHashValue("Yellow", cd) ? "True" : "False");
+	printf("Member Pink? %s\n", memberHashValue("Pink", cd) ? "True" : "False");
+	printf("Member Cyan? %s\n", memberHashValue("Cyan", cd) ? "True" : "False");
 	return 0;
 }
