f4290a94a6b15c76500935400f23e9fecee3cb86
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2024-07-05 14:56:17 +0800

    ADT BST - DFS Traversals Stage Completed

--- SUICO_BINARYSEARCHTREE_ACTIVITY/main.c
+++ SUICO_BINARYSEARCHTREE_ACTIVITY/main.c
@@ -4,6 +4,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void)
+{
+	NodePtr BTS;
+	INITIALIZEBST(&BTS);
+	POPULATEBST(&BTS);
+	
+	printf("Preorder traversal:\n");
+    DFSPREORDER(BTS);
+    printf("\n\nInorder traversal:\n");
+    DFSINORDER(BTS);
+    printf("\n\nPostorder traversal:\n");
+    DFSPOSTORDER(BTS);
 	return 0;
 }
