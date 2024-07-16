0958f274154627b542bcfe38ed5c472444e0559c
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-07-17 00:13:12 +0800

    ADT Graph Adjacency Matrix Updated

--- SUICO_GRAPH_ACTIVITY/Adjacency Matrix/AdjacencyMatrix.c
+++ SUICO_GRAPH_ACTIVITY/Adjacency Matrix/AdjacencyMatrix.c
@@ -2,34 +2,85 @@
 #include <stdlib.h>
 #include "AdjacencyMatrix.h"
 
-void displayAdjacencyMatrix(int adjacencyMatrix[][MAX])
+void addVertex(Graph *adjacencyMatrix)
 {
-	int x, y;
-	printf("Adjacency Matrix:\n\n");
-	printf("  ");
-	for (x = 0; x < MAX; x++) {
-		printf(" %c ", 'A' + x);
-	}
-	printf("\n");
-	for (x = 0; x < MAX; x++) {
-		printf("%c  ", 'A' + x);
-		for (y = 0; y < MAX; y++) {
-			printf("%d  ", adjacencyMatrix[x][y]);
-		}
-		printf("\n");
-	}
+    if (adjacencyMatrix->numberVertices == adjacencyMatrix->maxVertices) {
+        int newMaxVertices = adjacencyMatrix->maxVertices * 2;
+        int **newArray = (int **)malloc(sizeof(int*) * newMaxVertices);
+        
+        if (newArray == NULL) {
+            fprintf(stderr, "Memory allocation failed during resize\n");
+            exit(EXIT_FAILURE);
+        }
+        int x, y;
+        for (x = 0; x < newMaxVertices; x++) {
+            newArray[x] = (int *)malloc(sizeof(int) * newMaxVertices);
+        }
+        
+		for (x = 0; x < adjacencyMatrix->maxVertices; x++) {
+            for (y = 0; y < adjacencyMatrix->maxVertices; y++) {
+                newArray[x][y] = adjacencyMatrix->array[x][y];
+            }
+            free(adjacencyMatrix->array[x]);
+        }
+        free(adjacencyMatrix->array);
+        
+        for (x = adjacencyMatrix->maxVertices; x < newMaxVertices; x++) {
+            for (y = 0; y < newMaxVertices; y++) {
+                newArray[x][y] = 0;
+            }
+        }
+        for (x = 0; x < newMaxVertices; x++) {
+            for (y = adjacencyMatrix->maxVertices; y < newMaxVertices; y++) {
+                newArray[x][y] = 0;
+            }
+        }
+        
+        adjacencyMatrix->array = newArray;
+        adjacencyMatrix->maxVertices = newMaxVertices;
+    }
+    
+    adjacencyMatrix->numberVertices++;
 }
-void addEdge(int adjacencyMatrix[][MAX], int vertex1, int vertex2)
+
+void displayAdjacencyMatrix(Graph adjacencyMatrix)
 {
-	adjacencyMatrix[vertex1][vertex2] = 1;
-	adjacencyMatrix[vertex2][vertex1] = 1;
+    int x, y;
+    printf("Adjacency Matrix:\n");
+    printf("  ");
+    for (x = 0; x < adjacencyMatrix.numberVertices; x++) {
+        printf(" %c ", 'A' + x);
+    }
+    printf("\n");
+    for (x = 0; x < adjacencyMatrix.numberVertices; x++) {
+        printf("%c  ", 'A' + x);
+        for (y = 0; y < adjacencyMatrix.numberVertices; y++) {
+            printf("%d  ", adjacencyMatrix.array[x][y]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
-void initAdjacencyMatrix(int adjacencyMatrix[][MAX])
+
+void addEdge(Graph *adjacencyMatrix, int v1, int v2)
 {
-	int x, y;
-	for (x = 0; x < MAX; x++) {
-		for (y = 0; y < MAX; y++) {
-			adjacencyMatrix[x][y] = 0;
-		}
-	}
+    adjacencyMatrix->array[v1][v2] = 1;
+    adjacencyMatrix->array[v2][v1] = 1;
 }
+
+void initAdjacencyMatrix(Graph *adjacencyMatrix)
+{
+    adjacencyMatrix->maxVertices = 5;
+    adjacencyMatrix->array = (int **)malloc(sizeof(int*) * adjacencyMatrix->maxVertices);
+    int x, y;
+    for (x = 0; x < adjacencyMatrix->maxVertices; x++) {
+        adjacencyMatrix->array[x] = (int *)malloc(sizeof(int) * adjacencyMatrix->maxVertices);
+    }
+    for (x = 0; x < adjacencyMatrix->maxVertices; x++) {
+        for (y = 0; y < adjacencyMatrix->maxVertices; y++) {
+            adjacencyMatrix->array[x][y] = 0;
+        }
+    }
+    adjacencyMatrix->numberVertices = 0;
+}
+
